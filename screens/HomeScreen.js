e916e4dc847e07d30ebaf89ac319113c2ec031b6
selaguardia <selaguardia91@gmail.com>
2021-11-14 11:19:53 -0800

    get ride / food icons added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'twrnc'
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white h-full`}>
+    <SafeAreaView style={tw`bg-white`}>
       <View style={tw`p-5`}>
         <Image
           style={{
@@ -17,6 +18,7 @@ const HomeScreen = () => {
           }}
         />
       </View>
+      <NavOptions />
     </SafeAreaView>
   );
 };
