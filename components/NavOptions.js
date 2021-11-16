6dedd9dc97d16384ca7123362034199b7d61b81e
selaguardia <selaguardia91@gmail.com>
2021-11-15 17:53:50 -0800

    adds navigation card, able to set destination as well.

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,9 @@ import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import tw from 'twrnc'
 import { Icon } from 'react-native-elements'
 import { useNavigation } from "@react-navigation/core";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
+
 
 const data = [
   {
@@ -21,6 +24,7 @@ const data = [
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
   return (
     <FlatList
       data={data}
@@ -29,8 +33,9 @@ const NavOptions = () => {
       renderItem={({ item }) => (
         <TouchableOpacity 
           onPress={() => navigation.navigate(item.screen)}
-          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-          <View>
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}>
+          <View style={tw`${!origin && 'opacity-20'}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{ uri: item.image }}
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
-import Map from '../components/Map'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
   return (
-
-      <View>
-        <View style={tw`h-1/2`}>
-          <Map />
-        </View>
-        <View style={tw`h-1/2`}>
-          <Text>place map top half, info card bottom half</Text>
-        </View>
+    <View>
+      <View style={tw`h-1/2`}>
+        <Map />
       </View>
 
-  )
-}
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}  
+          >
+          </Stack.Screen>
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}  
+          >
+          </Stack.Screen>
+        </Stack.Navigator>
+      </View>
+    </View>
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
