a72ef22aa9c0d46ef6cb3cbdd471a5339f218e1d
selaguardia <selaguardia91@gmail.com>
2021-11-16 16:58:09 -0800

    adds distance and travel time to rideoptionscard.

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,14 +1,90 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import tw from "twrnc";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/core";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/navSlice";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-459",
+    title: "UberXL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "UberLUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={tw`flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-1 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-3 text-xl`}>Select a Ride - {travelTimeInformation?.distance.text}</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity 
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${id === selected?.id && "bg-gray-200"}`}>
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>{travelTimeInformation?.duration.text} travel time</Text>
+            </View>
+            <Text style={tw`text-xl`}>$30</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity 
+          disabled={!selected}
+          style={tw`bg-black py-3 mx-5 rounded-full ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
